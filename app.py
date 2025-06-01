80f77f446086c87cdd1522d6022d260aa72e7288
24f2001786 <24f2001786@ds.study.iitm.ac.in>
2025-06-01 11:38:07 +0530

    create logs folder and setup logging

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from models import db
 import os 
+import logging
+from logging.handlers import RotatingFileHandler
+
 
 app = Flask(__name__)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -10,6 +13,20 @@ app.config['SECRET_KEY'] = 'f56ae947f162336cb881dc9d354f3f4e6bb092c432065c3a'
 
 db.init_app(app)
 
+
+# --- Create Logs Folder & Setup Logging ---
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=3)
+file_handler.setFormatter(logging.Formatter(
+    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+))
+file_handler.setLevel(logging.INFO)
+
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('App started and logging is set up.')
 
 
 if __name__ == '__main__':
