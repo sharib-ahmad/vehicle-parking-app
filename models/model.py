5c12302139f11da2e089875726001e5ae91df424
24f2001786 <24f2001786@ds.study.iitm.ac.in>
2025-06-01 11:25:25 +0530

    created func for auto generete admin to the database

--- models/model.py
+++ models/model.py
@@ -1,5 +1,8 @@
 from models import db
+from flask import current_app
 from flask_login import UserMixin
+from sqlalchemy.exc import SQLAlchemyError
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import enum
 
@@ -28,4 +31,30 @@ class User(db.Model, UserMixin):
         return f'<User {self.full_name}>'
 
 
+# Function to create admin user when database is initialized
+def create_admin_user(app):
+    with app.app_context():
+        if not User.query.filter_by(role=UserRole.ADMIN).first():
+            try:
+                admin = User(
+                    id = '@sharib123',
+                    full_name='Sharib Ahmad',
+                    email = 'sharib@gmail.com',
+                    password_hash=generate_password_hash('admin123'),  # Use a secure password
+                    phone_number='1234567890',
+                    address='123 Admin St, Admin City',
+                    pin_code='123456',
+                    role=UserRole.ADMIN,
+                    is_active=True
+
+                )
+                db.session.add(admin)
+                db.session.commit()
+                current_app.logger.info(f'admin created successfully {admin.full_name}')
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                current_app.logger.error(f'error occured during created automatic admin to database {str(e)}')
+
+            finally:
+                db.session.close() 
 
