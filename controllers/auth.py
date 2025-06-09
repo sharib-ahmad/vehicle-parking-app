be82f1faac50f71e2e2cfcbffd257ddd51e2735d
24f2001786 <24f2001786@ds.study.iitm.ac.in>
2025-06-09 10:54:29 +0530

    adding logout function to auth.py

--- controllers/auth.py
+++ controllers/auth.py
@@ -87,7 +87,13 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
-
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    current_app.logger.info(f'You have been logged out.')
+    return redirect(url_for('local.index'))
 
 
 
