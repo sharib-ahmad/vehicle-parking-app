9df1cd50c0972c3394bb11946e988fd56d9bb3d3
24f2001786 <24f2001786@ds.study.iitm.ac.in>
2025-06-17 11:58:34 +0530

    create swaager.yaml file with the help of ai and some yt resources

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'vehicle_parking_app.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '5aae1d8d09362b9139d9c5a4'
-app.config['JWT_SECRET_KEY'] = '5aae1d8d09362b9139d9c5a4'
 
 
 # ╔═══════════════════════════════════════════════════════════════════════════╗
